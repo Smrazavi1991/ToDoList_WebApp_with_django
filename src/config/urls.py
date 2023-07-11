3d3fd6ee709f1b4b04a2548ce96443171061f032
Sayyed Mohammad Razavi <smrazavi1991@hotmail.com>
2023-07-11 16:01:32 +0330

    Initiate project routes

--- src/config/urls.py
+++ src/config/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ..todolist.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", Home.as_view(), name="Home-page"),
+    path("register/", Register.as_view(), name="Register"),
+    path("login/", Login.as_view(), name="Register")
 ]
