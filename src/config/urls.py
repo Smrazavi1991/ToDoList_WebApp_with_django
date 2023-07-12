7c77741680bb8eb43eed03824f2652aabc20fc05
Sayyed Mohammad Razavi <smrazavi1991@hotmail.com>
2023-07-12 22:41:03 +0330

    Correct the mistake in import views from an app

--- src/config/urls.py
+++ src/config/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from ..todolist.views import *
+from todolist.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
