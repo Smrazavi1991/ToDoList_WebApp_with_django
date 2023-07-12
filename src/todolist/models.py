f5fc09e6cd8a07fafaeb5b40501d7824c0d39ba8
Sayyed Mohammad Razavi <smrazavi1991@hotmail.com>
2023-07-12 22:39:40 +0330

    Add django_jalali library to models, change functionality of str method for User model, add verbose name plural for show ((categories)) instead of ((categorys)) in panel admin

--- src/todolist/models.py
+++ src/todolist/models.py
@@ -4,11 +4,12 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
+from django_jalali.db import models as jmodels
 
 
 def due_date_validator(value):
-    if datetime.datetime.now() > value:
-        raise ValidationError(f'You cannot set a task in past ({value})!!!')
+    if datetime.datetime.now().replace(tzinfo=datetime.timezone.utc) >= value:
+        raise ValidationError(f'You cannot set a task in passed times!!!')
 
 
 class BaseModel(models.Model):
@@ -16,14 +17,14 @@ class BaseModel(models.Model):
         abstract = True
 
     is_deleted = models.BooleanField('Is Deleted', default=False)
-    last_change = models.DateTimeField('Last Change', auto_now=True, editable=False)
-    create_date = models.DateTimeField('Create Date', auto_now_add=True, editable=False)
-    delete_datetime = models.DateTimeField('Delete Datetime', default=None, null=True, blank=True, editable=False)
+    last_change = jmodels.jDateTimeField('Last Change', auto_now=True, editable=False)
+    create_date = jmodels.jDateTimeField('Create Date', auto_now_add=True, editable=False)
+    delete_datetime = jmodels.jDateTimeField('Delete Datetime', default=None, null=True, blank=True, editable=False)
 
 
 class User(AbstractUser):
     email = models.EmailField('Email Address', unique=True)
-    date_joined = models.DateTimeField(_("date joined"), auto_now_add=True)
+    date_joined = jmodels.jDateTimeField(_("date joined"), auto_now_add=True)
 
     def get_full_name(self):
         """
@@ -33,18 +34,24 @@ class User(AbstractUser):
         return full_name.strip()
 
     def __str__(self):
-        return self.get_full_name()
+        return self.username
 
 
 class Category(BaseModel):
     name = models.CharField('Category Name', max_length=200)
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+    def __str__(self):
+        return self.name
 
 
 class Task(BaseModel):
     name = models.CharField('Name', max_length=200)
     description = models.CharField('Description', max_length=2000)
     category = models.ManyToManyField(Category)
-    due_date = models.DateTimeField('Due Date', null=True, validators=[due_date_validator])
+    due_date = jmodels.jDateTimeField('Due Date', null=True, validators=[due_date_validator])
     done = models.BooleanField('Done', default=False)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
